9725a69aaa3cf58e8f0ec5314cc9294f9dfa638e
cynthialin1228 <cynthialin1228@gmail.com>
2025-06-14 14:47:33 +0800

    Initial Commit

---
+++ BBTemp/BBTempApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBTempApp.swift
+//  BBTemp
+//
+//  Created by Cynthia on 2025/6/14.
+//
+
+import SwiftUI
+
+@main
+struct BBTempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBTempTests/BBTempTests.swift
@@ -0,0 +1,17 @@
+//
+//  BBTempTests.swift
+//  BBTempTests
+//
+//  Created by Cynthia on 2025/6/14.
+//
+
+import Testing
+@testable import BBTemp
+
+struct BBTempTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
